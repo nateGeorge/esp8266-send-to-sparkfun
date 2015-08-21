10c182f6c3e201eb0831377a995e7c54bcd4d415
wordsforthewise <wordsforthewise@gmail.com>
2015-08-21 00:25:21 -0500

    made it work

--- sendToSparkfun.lua
+++ sendToSparkfun.lua
@@ -1,7 +1,7 @@
 -- ***************************************************************************
 -- data.sparkfun posting module for ESP8266 with nodeMCU
 --
--- Written by Nathan George
+-- Written by Nate George
 --
 -- MIT license, http://opensource.org/licenses/MIT
 -- ***************************************************************************
@@ -12,43 +12,39 @@ _G[moduleName] = M
 
 local address = "54.86.132.254" -- IP for data.sparkfun.com
 
-if file.open('keys') then
-    local line = file.readline()
-    PuKey = string.sub(line,1,string.len(line)-1) --hack to remove CR/LF
-    line = file.readline()
-    PrKey = string.sub(line,1,string.len(line)-1)
-    file.close()
-
-function M.init(puKey, prKey)
-  i2c.setup(id, sda, scl, i2c.SLOW)
-  local calibration = read_reg(REG_CALIBRATION, 22)
-
-  AC1 = twoCompl(string.byte(calibration, 1) * 256 + string.byte(calibration, 2))
-  AC2 = twoCompl(string.byte(calibration, 3) * 256 + string.byte(calibration, 4))
-  AC3 = twoCompl(string.byte(calibration, 5) * 256 + string.byte(calibration, 6))
-  AC4 = string.byte(calibration, 7) * 256 + string.byte(calibration, 8)
-  AC5 = string.byte(calibration, 9) * 256 + string.byte(calibration, 10)
-  AC6 = string.byte(calibration, 11) * 256 + string.byte(calibration, 12)
-  B1 = twoCompl(string.byte(calibration, 13) * 256 + string.byte(calibration, 14))
-  B2 = twoCompl(string.byte(calibration, 15) * 256 + string.byte(calibration, 16))
-  MB = twoCompl(string.byte(calibration, 17) * 256 + string.byte(calibration, 18))
-  MC = twoCompl(string.byte(calibration, 19) * 256 + string.byte(calibration, 20))
-  MD = twoCompl(string.byte(calibration, 21) * 256 + string.byte(calibration, 22))
-
-  init = true
+local function loadKeys()
+    if file.open('keys') then
+        local line = file.readline()
+        PuKey = string.sub(line,1,string.len(line)-1) -- hack to remove CR/LF
+        line = file.readline()
+        PrKey = string.sub(line,1,string.len(line)-1)
+        file.close()
+    end
 end
 
-function sendData()
-
+function M.sendData(dataToSend, debug, pukey, prkey)
+    -- sendData is a table of data to send, with keys as the data labels in sparkfun
+    -- i.e. if you want to send some data to sparkfun which is labelled 'temperature',
+    -- you would make sendData = {'temperature'=70}
+    loadKeys()
+    pukey = pukey or PuKey
+    prkey = prkey or PrKey
+    debug = debug or false
     tmr.alarm(1,1000,1,function()
-        print("connecting")
+        if debug then
+            print("connecting")
+        end
         if (wifi.sta.status()==5) then
-            print("connected")
+            if debug then
+                print("connected")
+            end
             sk = net.createConnection(net.TCP, 0)
             sk:on("reconnection",function(conn) print("socket reconnected") end)
             sk:on("disconnection",function(conn) print("socket disconnected") end)
             sk:on("receive", function(conn, msg)
-                print(msg)
+                if debug then
+                    print(msg)
+                end
                 local success = nil
                 _,_,success = string.find(msg, "success")
                 if (success==nil) then
@@ -60,16 +56,21 @@ function sendData()
                 wifi.sta.disconnect()
                 end)
             sk:on("connection",function(conn) print("socket connected")
-                print("sending...")
+                if debug then
+                    print("sending...")
+                end
                 -- change the name of 05umprticles and 1um_paricles here, or change the GET request for a different server if needed
                 -- is not working right now...don't know why, it works as a string but not when joined...
-                sendStr = "GET /input/"..PuKey.."?private_key="..PrKey.."&fahrenheit="..currentDust.P2.."&mmhg="..currentDust.P1
+                sendStr = "GET /input/"..PuKey.."?private_key="..PrKey
+                for label, value in ipairs(sendData) do
+                    sendStr = sendStr.."&"..label.."="..value
+                end
                 conn:send(sendStr)
                 print(sendStr)
-                conn:send(" HTTP/1.1\r\n") 
+                conn:send(" HTTP/1.1\r\n")
                 conn:send("Host: "..address)
                 conn:send("Connection: close\r\n")
-                conn:send("Accept: */*\r\n") 
+                conn:send("Accept: */*\r\n")
                 conn:send("User-Agent: Mozilla/4.0 (compatible; ESP8266;)\r\n") 
                 conn:send("\r\n")
             end)
@@ -78,3 +79,5 @@ function sendData()
         end
     end)
 end
+
+return M
